7c4d7c5b570bf4aa7e787f727bfb32fd4cd7910a
Chloe Grace Moretz <shvetsovvitaliy06@gmail.com>
2018-04-04 22:25:12 +0300

    fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,24 @@
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            new Server();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class Main extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java!\n");
+    }
+
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()),"/*");
+        server.start();
+        server.join();
     }
 }
