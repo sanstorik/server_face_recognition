61e6e7e4b35240c3db552811acef7e869feab17a
Chloe Grace Moretz <shvetsovvitaliy06@gmail.com>
2018-04-07 21:40:03 +0300

    major database/query update

--- src/main/java/org/sanstorik/database/PostgreSqlConnection.java
+++ src/main/java/org/sanstorik/database/PostgreSqlConnection.java
@@ -2,27 +2,61 @@ package org.sanstorik.database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
+
 
 public class PostgreSqlConnection extends DatabaseConnection {
+    private static final String USERS_TABLE_NAME = "users";
 
     public PostgreSqlConnection() {
         super(System.getenv("JDBC_DATABASE_URL"));
     }
 
-    public String sqlQuery() {
-        executeSql("INSERT INTO USERS(NAME, PASSWORD) VALUES ('MATILDA', 'USER');");
 
-        String error = "";
+    public boolean checkLogin(String username, String password) {
+        ResultSet set = selectUserQuery(username, password);
+
         try {
-            ResultSet set = executeSqlQuery("SELECT * FROM USERS;");
-            set.next();
-
-            return set.getString("NAME");
+            return set != null && set.next();
         } catch (SQLException e) {
             e.printStackTrace();
-            error = e.toString();
         }
 
-        return error;
+        return false;
+    }
+
+
+    public void putToken(Token token) {
+        executeSql("insert into tokens(token, username, password) values(" +
+                "'"+ token.getToken() + "'," +
+                "'" + token.getUsername()+"'," +
+                "'" + token.getPassword() +"');"
+        );
+    }
+
+
+    public boolean isValidToken(Token token) {
+        ResultSet set = executeSqlQuery("select id, token, username, password from tokens " +
+                "where token='"+ token.getToken() + "';");
+
+        boolean isValid = false;
+
+        try {
+            isValid = set != null && set.next()
+                    && set.getString("username").equals(token.getUsername())
+                    && set.getString("password").equals(token.getPassword());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return isValid;
+    }
+
+
+    private ResultSet selectUserQuery(String username, String password) {
+        ResultSet set = executeSqlQuery("select id, username, password from " + USERS_TABLE_NAME +
+                " where username='"+ username +"' and password='"+ password +"';");
+
+        return set;
     }
 }
--- src/main/java/org/sanstorik/server/HttpServer.java
+++ src/main/java/org/sanstorik/server/HttpServer.java
@@ -1,16 +1,28 @@
 package org.sanstorik.server;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.sanstorik.database.PostgreSqlConnection;
+import org.sanstorik.database.Token;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpServer extends HttpServlet {
     private enum QueryMethod {
-        ACCEPT_JSON("/accept_json"), DECLINE_JSON("/decline_json"), NONE("/error");
+        LOGIN("/login"), ACCEPT_JSON("/accept_json"), DECLINE_JSON("/decline_json"),
+        UPLOAD_IMAGE("/upload_image"), NOT_SUPPORTED("/not_supported");
 
         private String stringRepresentation;
 
@@ -21,10 +33,14 @@ public class HttpServer extends HttpServlet {
         static QueryMethod of(String stringRepresentation) {
             if (stringRepresentation.equals(ACCEPT_JSON.stringRepresentation)) {
                 return ACCEPT_JSON;
+            } else if (stringRepresentation.equals(LOGIN.stringRepresentation)) {
+                return LOGIN;
             } else if (stringRepresentation.equals(DECLINE_JSON.stringRepresentation)) {
                 return DECLINE_JSON;
+            } else if (stringRepresentation.equals(UPLOAD_IMAGE.stringRepresentation)) {
+                return UPLOAD_IMAGE;
             } else {
-                return NONE;
+                return NOT_SUPPORTED;
             }
         }
 
@@ -34,6 +50,7 @@ public class HttpServer extends HttpServlet {
     }
 
     private final PostgreSqlConnection databaseConnection;
+    private String savedToken = "";
 
     public HttpServer() {
         super();
@@ -41,39 +58,192 @@ public class HttpServer extends HttpServlet {
         this.databaseConnection = new PostgreSqlConnection();
     }
 
+
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         getQuery(request, response);
     }
+
 
     @Override protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         postQuery(request, response);
     }
 
+
     private void getQuery(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        QueryMethod queryMethod = parseMethodByUrl(request.getRequestURL().toString());
+        String jsonResponse = "";
 
-        String msg = request.getMethod();
-        msg += request.getHeader("login");
-        msg += " " + request.getHeader("password");
+        switch (queryMethod) {
+            case LOGIN: {
+                jsonResponse = loginQuery(request);
+                break;
+            }
+            case ACCEPT_JSON: {
+                jsonResponse = acceptJsonQuery(request);
+                break;
+            }
+            case DECLINE_JSON: {
+                jsonResponse = declineJsonQuery();
+                break;
+            }
+            case NOT_SUPPORTED: {
+                jsonResponse = errorQuery();
+                break;
+            }
+        }
 
-        response.getWriter().print("\n " + msg);
-        response.getWriter().print("\n" + parseMethodByUrl(request.getRequestURL().toString()));
-        response.getWriter().print("\n" + request.getContextPath().toString());
-
-        response.getWriter().print("\n" + request.getParameter("name"));
-
-        response.getWriter().print("\n size = " + databaseConnection.sqlQuery());
+        response.getWriter().print(jsonResponse);
     }
+
+    private void postQuery(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        QueryMethod queryMethod = parseMethodByUrl(request.getRequestURL().toString());
+        String jsonResponse = "";
+
+        switch (queryMethod) {
+            case UPLOAD_IMAGE: {
+                jsonResponse = uploadImageQuery(request);
+                break;
+            }
+        }
+
+        response.getWriter().print(jsonResponse);
+    }
+
+
+    private String acceptJsonQuery(HttpServletRequest request) {
+        return "";
+    }
+
+
+    private String declineJsonQuery() {
+        return "";
+    }
+
+
+    private String errorQuery() {
+        return HttpResponse.error("Not supported method.").asJson();
+    }
+
+
+    private String uploadImageQuery(HttpServletRequest request) {
+        final String token = request.getHeader("token");
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("sanstorik_mangix");
+
+            DecodedJWT decoded = JWT.require(algorithm)
+                    .build().verify(token);
+
+            if (decoded.getExpiresAt().before(new Date())) {
+                return HttpResponse.error("Token is expired. Create a new one.").asJson();
+            }
+
+            if (!databaseConnection.isValidToken(
+                    new Token(token, decoded.getClaim("username").asString(),
+                            decoded.getClaim("password").asString()))) {
+                return HttpResponse.error("Token is not verified. Invalid user.").asJson();
+            }
+
+            Map<String, String> params = new HashMap<>();
+            params.put("image", "image");
+
+            return HttpResponse.create(params).asJson();
+        } catch (UnsupportedEncodingException|JWTDecodeException e) {
+            e.printStackTrace();
+        }
+
+        return HttpResponse.error("Failed to upload image. Check your token.").asJson();
+    }
+    
+    private String verifyToken(HttpServletRequest request) {
+        final String token = request.getHeader("token");
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("sanstorik_mangix");
+
+            DecodedJWT decoded = JWT.require(algorithm)
+                    .build().verify(token);
+
+            if (decoded.getExpiresAt().before(new Date())) {
+                return HttpResponse.error("Token is expired. Create a new one.").asJson();
+            }
+
+            if (!databaseConnection.isValidToken(
+                    new Token(token, decoded.getClaim("username").asString(),
+                            decoded.getClaim("password").asString()))) {
+                return HttpResponse.error("Token is not verified. Invalid user.").asJson();
+            }
+
+            Map<String, String> params = new HashMap<>();
+            params.put("image", "image");
+
+            return HttpResponse.create(params).asJson();
+        } catch (UnsupportedEncodingException|JWTDecodeException e) {
+            e.printStackTrace();
+        }
+
+        return HttpResponse.error("Failed to upload image. Check your token.").asJson();
+    }
+
+
+    private String loginQuery(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ((username == null || username.isEmpty())
+                || (password == null || password.isEmpty())) {
+            return HttpResponse.error("Params username and password are required.").asJson();
+        }
+
+        if (!databaseConnection.checkLogin(username, password)) {
+            return HttpResponse.error("Wrong password or login").asJson();
+        }
+
+        String authToken = createToken(username, password);
+
+        if (authToken == null) {
+            return HttpResponse.error("Couldn't create token for you.").asJson();
+        }
+
+        Map<String, String> responseParams = new HashMap<>(1);
+        responseParams.put("token", authToken);
+
+        return HttpResponse.create(responseParams).asJson();
+    }
+
+
+    private String createToken(String username, String password) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("sanstorik_mangix");
+            String token = JWT.create()
+                    .withClaim("username", username)
+                    .withClaim("password", password)
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 2 * 60 * 60 * 1000))
+                    .sign(algorithm);
+
+            //insert token to database
+            databaseConnection.putToken(new Token(token, username, password));
+
+            return token;
+        } catch (UnsupportedEncodingException|JWTCreationException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void checkToken() {
+
+    }
+
 
     private QueryMethod parseMethodByUrl(String url) {
         String methodName = url.substring(url.lastIndexOf("/"), url.length());
 
         return QueryMethod.of(methodName);
-    }
-
-    private void postQuery(HttpServletRequest request, HttpServletResponse response) {
-
     }
 }
