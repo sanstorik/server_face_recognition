abe0e47fc1b6d1c35df58d3ea15f301028ec28e5
Chloe Grace Moretz <shvetsovvitaliy06@gmail.com>
2018-04-12 04:27:14 +0300

    connected neutral network and face recognition to server

---
+++ src/main/java/org/sanstorik/neural_network/face_identifying/FaceFeatures.java
@@ -0,0 +1,57 @@
+package org.sanstorik.neural_network.face_identifying;
+
+
+public class FaceFeatures {
+
+    //128 features to characterize each face
+    private float[] features = new float[128];
+    private String faceLabel;
+    private long identifier;
+
+    {
+        faceLabel = String.valueOf("");
+    }
+
+    public FaceFeatures() { }
+
+
+    public FaceFeatures(float[] features) {
+        this.features = features;
+    }
+
+
+    public FaceFeatures(float[] features, String faceLabel) {
+        this.features = features;
+        this.faceLabel = faceLabel;
+    }
+
+
+    public void setFaceLabel(String faceLabel) {
+        this.faceLabel = faceLabel;
+    }
+
+
+    public String getFaceLabel() {
+        return faceLabel;
+    }
+
+
+    public void setFeatures(float[] features) {
+        this.features = features;
+    }
+
+
+    public float[] getFeatures() {
+        return features;
+    }
+
+
+    public void setIdentifier(long identifier) {
+        this.identifier = identifier;
+    }
+
+
+    public long getIdentifier() {
+        return identifier;
+    }
+}
